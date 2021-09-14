5bad44ff218f8e68bb0b14e6e1685188a2da70e9
Eddy <eddymartin0701@gmail.com>
2021-09-14 10:52:45 +0200

    first commit

---
+++ js/variable.js
@@ -0,0 +1,18 @@
+//Exemple of two variables with strings
+var userFirstname = "Eddy";
+var baseSentence = "Bonjour "
+
+// Display of the two variables in the console of the web browser
+console.log(baseSentence + userFirstname);
+console.log(baseSentence + userFirstname);
+console.log(baseSentence + userFirstname);
+console.log(baseSentence + userFirstname);
+
+var age = 15;
+console.log(age);
+age = age = 1;
+age += 1;
+age ++;
+console.log(age);
+
+var isAdult = false;
