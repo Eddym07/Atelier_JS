9cc935352d15e357785bbe30222ccc7a4adae0e8
Eddy <eddymartin0701@gmail.com>
2021-09-15 17:08:23 +0200

    functions

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+    console.log("\nFunctions");
+
+        // Déclaration de la fonction
+    function greetings(firstname, lastname) {
+        return"Bienvenue " + firstname + " " + lastname;
+    }
+
+        // Appelle de la fonctions pour déclaration
+    // greetings("Thomas", "Gossart");
+    // greetings("Sally", "Struters");
+    var speach = greetings("Bob", "L'Eponge");
+    console.log(speach);
+
