9734492decff9ae0ed66520580f2027af20241da
Eddy <eddymartin0701@gmail.com>
2021-09-15 13:24:02 +0200

    for of - for in

--- js/loops.js
+++ js/loops.js
@@ -22,13 +22,39 @@ var students = [
     "Timoté"
 ];
 
-while(i < students.length) {
-    console.log(students[i]);
-    i++;
+// while(i < students.length) {
+//     console.log(students[i]);
+//     i++;
+// }
+
+// console.log("\n##### Exemples de boucles 3 ##### \n");
+
+// for(var j = 0; j < students.length; j++)  {
+//     console.log(students[j]);
+// }
+
+for(var student of students) {
+    console.log(student);
+}
+for(var index in students) {
+    console.log(index)
 }
 
-console.log("\n##### Exemples de boucles 3 ##### \n");
+for(var index in students) {
+    console.log(student);
+}
 
-for(var j = 0; j < students.length; j++)  {
-    console.log(students[j]);
+for(var index in students) {
+    console.log("Etudiant numéro : " + (parseInt(index) + 1) + students[index]);
+}
+
+var human = {
+    "name" : "Henry",
+    "age" : 45,
+    "sexe" : "male",
+    "country" : "France"
+};
+
+for(var key in human) {
+    console.log(key + " : " + human[key]);
 }
